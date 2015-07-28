69995d51c74fe90972355c841c6df10433f19d61
q00148943 <q00148943@gmail.com>
2015-07-28 23:48:36 +0800

    Add some comment.

--- inc/list/list.h
+++ inc/list/list.h
@@ -15,20 +15,46 @@ struct ListNode {
 void deleteNode(struct ListNode* node);
 void reorderList(struct ListNode* head);
 
+/* #234	Palindrome Linked List*/
 bool isPalindrome(struct ListNode* head);
 
+/* #206	Reverse Linked List */
 struct ListNode* reverseList(struct ListNode* head);
+
+/* #19	Remove Nth Node From End of List */
 struct ListNode* removeNthFromEnd(struct ListNode* head, int n);
+
+/* #203	Remove Linked List Elements */
 struct ListNode* removeElements(struct ListNode* head, int val);
+
+/* #83	Remove Duplicates from Sorted List */
 struct ListNode* deleteDuplicates(struct ListNode* head);
+
+/* #21	Merge Two Sorted Lists */
 struct ListNode* mergeTwoLists(struct ListNode* l1, struct ListNode* l2);
+
+/* #160	Intersection of Two Linked Lists */
 struct ListNode* getIntersectionNode(struct ListNode *headA, struct ListNode *headB);
+
+/* #24	Swap Nodes in Pairs */
 struct ListNode* swapPairs(struct ListNode* head);
+
+/* #61	Rotate List */
 struct ListNode* rotateRight(struct ListNode* head, int k);
+
+/* #2	Add Two Numbers*/
 struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2);
+
+/* #147	Insertion Sort List*/
 struct ListNode* insertionSortList(struct ListNode* head);
+
+/* #92	Reverse Linked List II */
 struct ListNode* reverseBetween(struct ListNode* head, int m, int n);
+
+/* #82	Remove Duplicates from Sorted List II*/
 struct ListNode* deleteDuplicates2(struct ListNode* head);
+
+/* #148	Sort List*/
 struct ListNode* sortList(struct ListNode* head);
 
 /* #86	Partition List */

5dcbc12c083066fed4341707aa94c3cb1e857469
q00148943 <q00148943@gmail.com>
2015-10-03 08:39:25 +0800

    Finish the Remove Duplicates from Sorted Array problem

--- inc/array/array.h
+++ inc/array/array.h
@@ -3,4 +3,13 @@
 
 char** summaryRanges(int* nums, int numsSize, int* returnSize);
 
+/* #26    Remove Duplicates from Sorted Array */
+int removeDuplicates(int* nums, int numsSize);
+
+/* #27    Remove Element */
+int removeElement(int* nums, int numsSize, int val);
+
+/* #189    Rotate Array */
+void rotate(int* nums, int numsSize, int k);
 #endif /* ARRAY_H */
+
--- src/array/array.c
+++ src/array/array.c
@@ -8,3 +8,55 @@ char** summaryRanges(int* nums, int numsSize, int* returnSize)
     return NULL;
 }
 
+int removeDuplicates(int* nums, int numsSize)
+{
+    if ((nums == NULL) || (numsSize == 0)) {
+        return 0;
+    }
+
+    int elem = nums[0];
+    int idx  = 1;
+    
+    for (int i = 1; i < numsSize; i++) {
+        if (nums[i] != elem) {
+            nums[idx++] = nums[i];
+            elem = nums[i];
+        }
+    }
+
+    return idx;
+}
+
+int removeElement(int* nums, int numsSize, int val)
+{
+    if ((nums == NULL) || (numsSize == 0)) {
+        return 0;
+    }
+
+    int idx = 0;
+    
+    for (int i = 0; i < numsSize; i++) {
+        if (nums[i] != val) {
+            nums[idx++] = nums[i];
+        }
+    }
+
+    return idx;
+}
+
+void rotate(int* nums, int numsSize, int k)
+{
+    if ((nums == NULL) || (numsSize == 0)) {
+        return;
+    }
+
+    int rotate = k%numsSize;
+
+    for (int i = 0; i < rotate; i++) {
+        nums[i] = nums[numsSize - rotate + i];
+    }
+
+
+    return;
+}
+
